8eae9a9512606b0d87297223577ff488bfa4209f
nix <17600080426@163.com>
2021-12-12 15:32:04 +0800

    1111

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,14 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    首页
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: "index"
 }
 </script>
 
-<style>
+<style scoped>
+
 </style>
